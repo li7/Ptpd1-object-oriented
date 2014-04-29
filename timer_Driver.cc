04dc9461bdc84f2b9bd1f2623e09b43d389b51ad
li7 <li7@hawaii.edu>
2014-04-29 09:41:53 -1000

    updated drivers

--- timer_Driver.cc
+++ timer_Driver.cc
@@ -1,4 +1,5 @@
 #include "classes/IntervalTimer.h"
+#include "classes/ptpd_dep.h"
 #include <iostream>
 #include <string.h>
 
@@ -6,6 +7,26 @@ using namespace std;
 
 main()
 {
-	IntervalTimer timer;
+	IntervalTimer itimer[TIMER_ARRAY_SIZE];
+	int sig = 0;
+	Boolean b;
+	int i;
+
+	catch_alarm(sig);
+	initTimer();
+	timerStart(3,2,itimer);
+	timerUpdate(itimer);
+	b = timerExpired(3,itimer);
+	cout << "timer expired " << b << endl;
+	for(i=0;i<TIMER_ARRAY_SIZE;++i)
+	{
+		itimer[i].set_left(itimer[i].get_interval());
+		itimer[i].set_expire(true);
+	}
+	timerUpdate(itimer);
+	b = timerExpired(0,itimer);
+	cout << "timer expired " << b << endl;
+	timerStop(0,itimer);
+
 }
 
